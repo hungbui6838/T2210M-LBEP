c80bca25d4727f10e76f285348f17eaf275bde8d
hùng bùi <hungbuinc2003@gmail.com>
2022-10-31 08:20:01 +0700

    btvn

---
+++ btvn5/Untitled1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int x=0; 
+	int arr[n]; 
+		for(int i= 0;i<n;i++){
+			while(arr[i]%2!=0){
+				int tsl; 
+				tsl+=arr[i];
+				x++; 
+			}
+			int tbsl,tsl; 
+			tbsl=tsl/x;
+			return tbsl; 
+		}  
+} 
